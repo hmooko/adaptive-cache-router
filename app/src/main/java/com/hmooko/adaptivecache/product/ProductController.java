6e65bba217ad78445b4788a54123353f0bede267
HyunoMo Koo <103720259+hmooko@users.noreply.github.com>
2026-09-23 08:49:32 +0900

    feat: expose product read API

---
+++ app/src/main/java/com/hmooko/adaptivecache/product/ProductController.java
@@ -0,0 +1,22 @@
+package com.hmooko.adaptivecache.product;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/{productId}")
+    public ProductResponse getProduct(@PathVariable long productId) {
+        return productService.getProduct(productId);
+    }
+}
