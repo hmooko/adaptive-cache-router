10256121200c0ecafcc6fed8eb6b19586a5d9c1a
HyunoMo Koo <103720259+hmooko@users.noreply.github.com>
2026-09-23 08:49:18 +0900

    feat: add Spring Boot entrypoint

---
+++ app/src/main/java/com/hmooko/adaptivecache/AdaptiveCacheRouterApplication.java
@@ -0,0 +1,12 @@
+package com.hmooko.adaptivecache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaptiveCacheRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdaptiveCacheRouterApplication.class, args);
+    }
+}
